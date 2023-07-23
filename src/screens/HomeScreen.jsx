fa4a9bb9b51e5b8b0eaf60cecaf979bfe2dac6fe
Gonzalo Yacante <gyacante9@gmail.com>
2023-07-24 00:11:55 +0100

    Cambio en HombreScreen.jsx
    eliminacion del boton (conoceme mas) por desuso

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,7 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
-import Stack from "react-bootstrap/Stack";
 
 import astronaut_green from "../assets/astronaut_green.svg";
 
@@ -11,20 +10,15 @@ export const HomeScreen = () => {
       id="Home"
       fluid
       className="align-items-center justify-content-center d-flex flex-column min-vh-100">
-      <p className="h5">Hola, yo soy</p>
+      <p className="fs-5 m-1">Hola, yo soy</p>
       <h1 className="text-primary fs-1">
         <strong>Gonzalo Yacante</strong>
       </h1>
-      <p className="h4">Desarrollador Frontend</p>
+      <p className="fs-4">Desarrollador Frontend</p>
 
-      <Stack direction="horizontal" gap={3} className="justify-content-center mt-3">
-        <Button variant="outline-primary">
-          Descargar CV
-        </Button>
-        <Button href="#AboutMe" variant="primary">
-          Conóceme más
-        </Button>
-      </Stack>
+      <Button variant="outline-primary">
+        Descargar CV
+      </Button>
       <Image width={200} src={astronaut_green} />
     </Container>
   );
