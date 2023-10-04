17ec5fb632270c5106a90ac1cca7133cc1371516
Gonzalo Yacante <gyacante9@gmail.com>
2023-10-04 15:51:21 +0100

    Probando fallos del formulario

--- src/screens/ContactScreen.jsx
+++ src/screens/ContactScreen.jsx
@@ -1,84 +1,44 @@
-import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 
 export const ContactScreen = () => {
-  const [validated, setValidated] = useState(false);
-
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    } else {
-      form.setAttribute("onSubmit", "submit"); // Agregar el atributo onSubmit
-    }
-
-    setValidated(true);
-  };
   return (
     <section className="my-5" id="Skills">
       <h2>Contáctame</h2>
       <Form
         name="contact"
-        method="post"
+        method="POST"
         data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onChange={handleSubmit}
-        className="my-4 px-2"
-        noValidate
-        validated={validated}>
-        <Row className="flex-column">
-          <input type="hidden" name="form-name" value="contact" />
-          <Form.Group
-            className="mb-3"
-            as={Col}
-            md={{ span: 6, offset: 3 }}
-            controlId="validationCustom01">
-            <Form.Label>Nombre completo</Form.Label>
-            <Form.Control
-              name="name"
-              required
-              type="text"
-              placeholder="Gonzalo Yacante"
-            />
-            <Form.Control.Feedback>Suena bien!</Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group
-            className="mb-3"
-            as={Col}
-            md={{ span: 6, offset: 3 }}
-            controlId="validationCustom02">
-            <Form.Label>Correo electrónico</Form.Label>
-            <Form.Control
-              name="email"
-              required
-              type="email"
-              placeholder="ejemplo@correo.com"
-            />
-            <Form.Control.Feedback>Perfecto!</Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group
-            className="mb-3"
-            as={Col}
-            md={{ span: 6, offset: 3 }}
-            controlId="validationCustom03">
-            <Form.Label>Mensaje</Form.Label>
-            <Form.Control
-              name="message"
-              required
-              as="textarea"
-              placeholder="Explícame brevemente tu idea."
-              rows={4}
-            />
-            <Form.Control.Feedback>Buena idea!</Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group className="mb-3" as={Col} md={{ span: 6, offset: 3 }}>
-            <Button type="submit">Enviar</Button>
-          </Form.Group>
-        </Row>
+        onSubmit="submit"
+      >
+        <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+          <Form.Label column sm={2}>
+            Email
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="email" placeholder="Email" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group
+          as={Row}
+          className="mb-3"
+          controlId="formHorizontalPassword">
+          <Form.Label column sm={2}>
+            Password
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Control type="password" placeholder="Password" />
+          </Col>
+        </Form.Group>
+
+        <Form.Group as={Row} className="mb-3">
+          <Col sm={{ span: 10, offset: 2 }}>
+            <Button type="submit">Sign in</Button>
+          </Col>
+        </Form.Group>
       </Form>
     </section>
   );
