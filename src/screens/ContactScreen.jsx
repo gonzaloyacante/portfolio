9d1494ecdb48771dee05161f51c994d8833ff220
Gonzalo Yacante <gyacante9@gmail.com>
2023-10-04 17:11:23 +0100

    Formulario de ContactScreen terminado

--- src/screens/ContactScreen.jsx
+++ src/screens/ContactScreen.jsx
@@ -1,35 +1,88 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+
 export const ContactScreen = () => {
+  const [validated, setValidated] = useState(false);
+
+  const handleChange = () => {
+    setValidated(true);
+  };
   return (
-    <section className="my-5" id="Skills">
+    <section className="mt-5" id="Skills">
       <h2>Contáctame</h2>
-      <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
-        <input type="hidden" name="form-name" value="contact" />
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email address
-            <input
+      <Form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onChange={handleChange}
+        onSubmit="submit"
+        className="my-4 px-2"
+        noValidate
+        validated={validated}>
+        <Row className="flex-column">
+          <input type="hidden" name="form-name" value="contact" />
+          <Form.Group
+            className="mb-3"
+            as={Col}
+            xs={{ span: 10, offset: 1 }}
+            md={{ span: 8, offset: 2 }}
+            lg={{ span: 6, offset: 3 }}
+            controlId="name">
+            <Form.Label>Nombre completo</Form.Label>
+            <Form.Control
+              name="name"
+              required
+              type="text"
+              placeholder="Gonzalo Yacante"
+            />
+            <Form.Control.Feedback>Suena bien!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group
+            className="mb-3"
+            as={Col}
+            xs={{ span: 10, offset: 1 }}
+            md={{ span: 8, offset: 2 }}
+            lg={{ span: 6, offset: 3 }}
+            controlId="email">
+            <Form.Label>Correo electrónico</Form.Label>
+            <Form.Control
               name="email"
+              required
               type="email"
-              className="form-control"
-              id="email"
+              placeholder="ejemplo@correo.com"
             />
-          </label>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-            <input
-              name="password"
-              type="password"
-              className="form-control"
-              id="password"
+            <Form.Control.Feedback>Perfecto!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group
+            className="mb-3"
+            as={Col}
+            xs={{ span: 10, offset: 1 }}
+            md={{ span: 8, offset: 2 }}
+            lg={{ span: 6, offset: 3 }}
+            controlId="message">
+            <Form.Label>Mensaje</Form.Label>
+            <Form.Control
+              name="message"
+              required
+              as="textarea"
+              placeholder="Explícame brevemente tu idea."
+              rows={4}
             />
-          </label>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+            <Form.Control.Feedback>Buena idea!</Form.Control.Feedback>
+          </Form.Group>
+          <Col
+            className="mb-3"
+            xs={{ span: 10, offset: 1 }}
+            md={{ span: 8, offset: 2 }}
+            lg={{ span: 6, offset: 3 }}>
+            <Button type="submit">Enviar</Button>
+          </Col>
+        </Row>
+      </Form>
     </section>
   );
 };
